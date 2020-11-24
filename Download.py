c9c8ee683730839e1caed827b7575fccff7836ef
Abhishek Bhargav <39580132+abhibhargav29@users.noreply.github.com>
2020-11-24 09:13:45 +0530

    Update Download.py

--- Download.py
+++ Download.py
@@ -1,13 +1,18 @@
 from pytube import YouTube
 import os
 
+#Saved in same location
 SAVE_PATH = os.getcwd()
+
+#User input for filename and link
 fileName = input("Enter Save File Name:")
 link = input("Enter link of You Tube video:")
 
+#Create object of you tube class
 yt = YouTube(link) 
 mp4files = yt.streams.filter(progressive=True, file_extension="mp4") 
   
+#Try downloading
 try:
     mp4files.get_highest_resolution().download(output_path=SAVE_PATH, filename=fileName) 
     print("Task Completed!")
